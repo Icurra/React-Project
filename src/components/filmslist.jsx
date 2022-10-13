483acda1667c6c3eb09f914db774713ab874b9ed
Icurra <icurrablade@gmail.com>
2022-10-13 12:42:10 -0700

    Hooks Excercise

--- src/components/filmslist.jsx
+++ src/components/filmslist.jsx
@@ -1,33 +1,26 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 
-class FilmsList extends Component {
-  constructor(props) {
-    super(props);
+function FilmsList (props) {
+  let [list, setList] = useState([])
 
-    this.state = {
-      list: [],
-    };
-  }
-
-  getFilms() {
+ function getFilms() {
     fetch("https://ghibliapi.herokuapp.com/films")
       .then((response) => response.json())
-      .then((films) => this.setState({ list: films }))
+      .then((films) => setList(films))
       .catch((error) => console.log(error));
   }
 
-  componentDidMount() {
-    this.getFilms();
-  }
-  render() {
+  useEffect(() => {
+    getFilms();
+  }, [])
+ 
     return (
       <ul>
-        {this.state.list.map((film) => {
+        {list.map((film) => {
           return <li key={film.id}>{film.title}</li>;
         })}
       </ul>
     );
-  }
 }
 
 export default FilmsList;
